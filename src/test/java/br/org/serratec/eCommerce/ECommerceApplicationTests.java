c63ad4c513c17ea365294bc15c2a6222a36c6d3f
Emanuel_Fonse <efartecontato@gmail.com>
2024-05-20 21:21:41 -0300

    First Commit

---
+++ src/test/java/br/org/serratec/eCommerce/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.serratec.eCommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
